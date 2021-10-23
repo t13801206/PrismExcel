52846dd6f898947030d00b2cd204437de9bd52c9
t13801206 <t13801206@yahoo.co.jp>
2021-10-23 19:44:51 +0900

    テーブルに初期データを表示した

--- PrismExcel/PrismExcel/ViewModels/MainTableViewModel.cs
+++ PrismExcel/PrismExcel/ViewModels/MainTableViewModel.cs
@@ -4,14 +4,23 @@ using Prism.Regions;
 using PrismExcel.Core;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Windows.Data;
 
 namespace PrismExcel.ViewModels
 {
     public class MainTableViewModel : BindableBase
     {
+        public ObservableCollection<Models.Contact> Contacts { get; set; }
+
+        private readonly ICollectionView _view;
+
         public MainTableViewModel()
         {
+            Contacts = new ObservableCollection<Models.Contact>(Models.Contact.GenerateInitList());
+            _view = (CollectionView)CollectionViewSource.GetDefaultView(Contacts);
         }
     }
 }
