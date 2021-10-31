1515a3c9defc728dae518d9c0f1259955fb853d9
t13801206 <t13801206@yahoo.co.jp>
2021-10-31 20:08:46 +0900

    とりあえず動く

---
+++ PrismExcel/PrismExcel.Core/CustomAttachedProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+namespace PrismExcel.Core
+{
+    [Obsolete("列インデックスのために使おうとして断念した")]
+    public class SampleAttachedProperties
+    {
+        public static int GetColmunIndex(DependencyObject obj)
+        {
+            return (int)obj.GetValue(ColmunIndexProperty);
+        }
+
+        public static void SetColmunIndex(DependencyObject obj, int value)
+        {
+            obj.SetValue(ColmunIndexProperty, value);
+        }
+
+        public static readonly DependencyProperty ColmunIndexProperty =
+            DependencyProperty.RegisterAttached("ColmunIndex", typeof(int), typeof(SampleAttachedProperties), new PropertyMetadata(0));
+    }
+}
--- PrismExcel/PrismExcel/ViewModels/FilterDialogViewModel.cs
+++ PrismExcel/PrismExcel/ViewModels/FilterDialogViewModel.cs
@@ -1,34 +1,48 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
+﻿using Prism.Mvvm;
 using Prism.Services.Dialogs;
+using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using PrismExcel.Services.Interfaces;
+using System.Collections.ObjectModel;
 
 namespace PrismExcel.ViewModels
 {
     public class FilterDialogViewModel : BindableBase, IDialogAware
     {
+        public ReactiveCommand OkCommand { get; } = new ReactiveCommand();
+
+        public ReactiveCommand CancelCommand { get; } = new ReactiveCommand();
+
+        public List<IFilterItem> ItemSource { get; set; } = new List<IFilterItem>();
+
         public FilterDialogViewModel()
         {
+            OkCommand.Subscribe(() => 
+            {
+                RequestClose.Invoke(new DialogResult(ButtonResult.OK));
+            });
 
+            CancelCommand.Subscribe(() => 
+            {
+                RequestClose.Invoke(new DialogResult(ButtonResult.Cancel));
+            });
         }
 
-        public string Title => "";
+        public string Title => "列のフィルタリング";
 
         public event Action<IDialogResult> RequestClose;
 
-        public bool CanCloseDialog()
-        {
-            return true;
-        }
+        public bool CanCloseDialog() => true;
 
-        public void OnDialogClosed()
-        {
-        }
+        public void OnDialogClosed() { }
 
         public void OnDialogOpened(IDialogParameters parameters)
         {
+            var filterItems = parameters.GetValue<ReadOnlyCollection<IFilterItem>>("filterItems");
+
+            ItemSource.Clear();
+            ItemSource.AddRange(filterItems);
         }
     }
 }
